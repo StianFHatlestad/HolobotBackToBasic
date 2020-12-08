d5b6d368a983c244bebd4099016b41a0d607e576
Raoryn <qaz614@hotmail.com>
2020-12-08 13:20:13 +0100

    Update main.cpp
    added IRremote.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <Servo.h>
-
+#include <IRremote.h>
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 int pos = 0;    // variable to store the servo position
